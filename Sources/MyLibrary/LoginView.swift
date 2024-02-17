370de10bbbb3a4a39d7276c2a84de2f9cee0ec6b
yashChouhanGit <yashchouhan@371@gmail.com>
2024-02-17 19:56:56 +0530

    first commit

---
+++ Sources/MyLibrary/LoginView.swift
@@ -0,0 +1,33 @@
+//
+//  LoginView.swift
+//  
+//
+//  Created by mac on 17/02/24.
+//
+
+import UIKit
+
+class LoginView: UIView {
+ 
+    @IBOutlet var contentView: UIView!
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configuration()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        configuration()
+    }
+    
+    func configuration() {
+       Bundle.main.loadNibNamed("LoginView", owner: self, options: nil)
+        self.addSubview(contentView)
+        contentView.translatesAutoresizingMaskIntoConstraints = true
+        contentView.topAnchor.constraint(equalTo: self.topAnchor)
+        contentView.bottomAnchor.constraint(equalTo: self.bottomAnchor)
+        contentView.leadingAnchor.constraint(equalTo: self.leadingAnchor)
+        contentView.trailingAnchor.constraint(equalTo: self.trailingAnchor)
+    }
+}
